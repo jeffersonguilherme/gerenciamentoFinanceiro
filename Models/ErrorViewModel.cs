4a3d1940dc1dd11b9c4a414b6e5a31fe009a5f8e
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2025-08-30 20:43:33 -0300

    Primeiro Commite

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gerenciamentoFinanceiro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
